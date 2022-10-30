87b518cf4cc038cdd89da047c41535184fbe1e19
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-10-30 11:09:58 +0200

    Add SauceKey

--- Sources/Lowtech/Overrides.swift
+++ Sources/Lowtech/Overrides.swift
@@ -1,6 +1,9 @@
 import Path
+import Sauce
 
 public typealias CustomFilePath = Path
 public func p(_ string: String) -> CustomFilePath? {
     CustomFilePath(string)
 }
+
+public typealias SauceKey = Key
