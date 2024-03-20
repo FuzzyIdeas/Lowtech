630c8825e015874348d80cf3716fff6794404f1e
Alin Panaitiu <alin.panaitiu@gmail.com>
2024-03-20 21:34:08 +0200

    Updates

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
         .package(url: "https://github.com/diniska/swiftui-system-colors", from: "1.1.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "6.3.1"),
         .package(url: "https://github.com/marcprux/MemoZ.git", from: "1.3.0"),
-        .package(url: "https://github.com/IdeasOnCanvas/AppReceiptValidator.git", branch: "main"),
+        .package(url: "https://github.com/alin23/AppReceiptValidator", from: "1.2.0"),
         .package(url: "https://github.com/Kitura/BlueECC", branch: "master"),
 
         .package(url: "https://github.com/alin23/PaddleSPM", branch: "main"),
