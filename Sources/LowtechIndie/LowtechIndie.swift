eb55a364c70a303cda0daee9d5a41f65aa967cec
Alin Panaitiu <alin.panaitiu@gmail.com>
2025-01-27 23:37:59 +0200

    Make public

--- Sources/LowtechIndie/LowtechIndie.swift
+++ Sources/LowtechIndie/LowtechIndie.swift
@@ -4,13 +4,13 @@ import Sparkle
 
 // MARK: - UpdateCheckInterval
 
-enum UpdateCheckInterval: Int {
+public enum UpdateCheckInterval: Int {
     case daily = 86400
     case everyThreeDays = 259_200
     case weekly = 604_800
 }
 
-extension Defaults.Keys {
+public extension Defaults.Keys {
     static let silentUpdates = Key<Bool>("SUAutomaticallyUpdate", default: false)
     static let checkForUpdates = Key<Bool>("SUEnableAutomaticChecks", default: true)
     static let updateCheckInterval = Key<Int>("SUScheduledCheckInterval", default: 86400)
